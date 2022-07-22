c311185dea7a35252f68f38ce4e62137bba60f13
Ilia Ivko <mrloking11@gmail.com>
2022-07-22 22:46:25 +0200

    Add models Label, Question, Answer and register new models in admin.py

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+@admin.register( Label )
+class LabelAdmin( admin.ModelAdmin ):
+    """
+        Label model class for admin panel
+    """
+
+    list_display = ( 'name', 'creator',)
+
+@admin.register( Question )
+class QuestionAdmin( admin.ModelAdmin ):
+    """
+        Question model class for admin panel
+    """
+
+    list_display = ( 'title', 'date_of_creation', 'creator', )
+    list_filter = ( 'title', 'date_of_creation', )
+    readonly_fields = ( 'date_of_creation', )
+
+    fieldsets = (
+        ( 'General', {
+            'fields' : ( ( 'title', ), ( 'date_of_creation', 'creator', ), )
+        }),
+
+        ( 'Additionaly', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( ( 'content', ), ( 'labels', ), )
+        })
+    )
+
+@admin.register( Answer )
+class AnswerAdmin( admin.ModelAdmin ):
+    """
+        Answer model class for admin panel
+    """
+
+    list_display = ( 'date_of_creation', 'creator', 'question' )
+    list_filter = ( 'date_of_creation', )
+    readonly_fields = ( 'date_of_creation', )
+
+    fieldsets = (
+        ( 'General', {
+            'fields' : ( ( 'date_of_creation', ), ( 'question', 'creator', ), )
+        }),
+
+        ( 'Additionaly', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( 'content', )
+        })
+    )
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,100 @@
 from django.db import models
 
-# Create your models here.
+from user_controller.models import CustomUser
+
+
+
+class Label( models.Model ):
+    """
+        Forum labels model class
+    """
+
+    name = models.CharField(
+        max_length = 64,
+    )
+
+    creator = models.ForeignKey(
+        to = CustomUser,
+        on_delete = models.SET_NULL,
+        related_name = 'labels',
+
+        null = True,
+    )
+
+
+    def __str__(self) -> str:
+        return f'Label: {self.name}'
+
+
+    class Meta:
+        ordering = [ 'name', ]
+
+
+class Question( models.Model ):
+    """
+        Forum questions model class
+    """
+
+    title = models.CharField(
+        max_length = 255,
+        unique = True,
+    )
+    content = models.TextField()
+
+    date_of_creation = models.DateTimeField(
+        auto_now_add = True,
+    )
+
+    creator = models.ForeignKey(
+        to = CustomUser,
+        on_delete = models.SET_NULL,
+        related_name = 'questions',
+
+        null = True,
+    )
+    labels = models.ManyToManyField(
+        to = Label,
+        related_name = 'questions',
+    )
+
+
+    def __str__(self) -> str:
+        return f'Question: {self.title}'
+
+
+    class Meta:
+        ordering = [ 'date_of_creation', ]
+
+
+class Answer( models.Model ):
+    """
+        Question answers model class
+    """
+
+    content = models.TextField()
+
+    date_of_creation = models.DateTimeField(
+        auto_now_add = True,
+    )
+
+    creator = models.ForeignKey(
+        to = CustomUser,
+        on_delete = models.SET_NULL,
+        related_name = 'answers',
+
+        null = True,
+    )
+    question = models.ForeignKey(
+        to = Question,
+        on_delete = models.CASCADE,
+        related_name = 'questions',
+    )
+
+
+    def __str__(self) -> str:
+        return f'Answer: {self.pk}'
+
+
+    class Meta:
+        ordering = [ 'date_of_creation', ]
+
