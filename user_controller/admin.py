fc81295672dfe9331c106b006de1cac8017b3601
Ilia Ivko <mrloking11@gmail.com>
2022-07-22 18:20:41 +0200

    Add 'User custom model class for admin panel'

--- user_controller/admin.py
+++ user_controller/admin.py
@@ -6,4 +6,21 @@ from .models import *
 
 @admin.register( CustomUser )
 class CustomUserAdmin( admin.ModelAdmin ):
-    pass
+    """
+        User custom model class for admin panel
+    """
+
+    list_display = ( 'username', 'registration_date', 'first_name', 'last_name', 'gender', 'birth_date' )
+    list_filter = ( 'registration_date', )
+    readonly_fields = ( 'registration_date', )
+
+    fieldsets = (
+        ( 'Общее', {
+            'fields' : ( ( 'username', ), ( 'registration_date' ), )
+        }),
+
+        ( 'О пользователе', {
+            'classes' : ( 'collapse', ),
+            'fields' : ( ('first_name', 'last_name'), 'gender', 'birth_date' )
+        })
+    )
