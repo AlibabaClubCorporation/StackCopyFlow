57aaa5d9a471909080ff533da27aebf0eb750d63
Ilia Ivko <mrloking11@gmail.com>
2022-08-07 02:35:23 +0200

    Small Debug

--- user_opinion_controller/signals.py
+++ user_opinion_controller/signals.py
@@ -3,11 +3,11 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Group
 
 from .models import AppealToUser
+from.services.service_of_appeal import UserBannedController
 
 
 
 @receiver( post_save, sender = AppealToUser )
 def check_auto_banned( sender, instance, created, **kwargs ):
     if AppealToUser.objects.filter( receiver = instance.receiver ).count() > 4:
-        instance.receiver.groups.clear()
-        instance.receiver.groups.add( Group.objects.get( name = 'banned' ) )
+        UserBannedController.user_blocking( instance.receiver )
